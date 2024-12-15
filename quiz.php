c5bea0ac3e0f3033aa95cb4eff0ddd3c5f172dee
webdevelopersumon <wdevelopersumon@gmail.com>
2024-12-15 11:42:04 +0600

    Upload problem
    Upload problem
    
    Signed-off-by: webdevelopersumon <wdevelopersumon@gmail.com>

---
+++ quiz.php
@@ -0,0 +1,49 @@
+<?php
+
+$questions = [
+
+    ['question' => 'What is 2 + 2?', 'correct' => '4'],
+
+    ['question' => 'What is the capital of France?', 'correct' => 'Paris'],
+
+    ['question' => 'Who wrote "Hamlet"?','correct' => 'Shakespeare'],
+
+];
+
+$answers = [];
+
+    foreach ($questions as $index => $question) {
+ echo ($index + 1) . ". " . $question['question'] . "\n";
+
+     $answers[] = trim(readline("Your answer: "));
+
+ }
+function evaluateQuiz(array $questions, array $answers): int{
+    $score = 0;
+
+    foreach ($questions as $index => $question) {
+
+        if ($answers[$index] === $question['correct']) {
+         $score++;
+
+        }
+
+     }
+        return $score;
+}
+//score
+ $score = evaluateQuiz($questions, $answers);
+
+         echo "\nYou scored $score out of " . count($questions) . ".\n";
+
+
+ if ($score === count($questions)) {
+
+    echo "Excellent job!\n";
+
+    } elseif ($score > 1) {
+        echo "Good effort!\n";
+
+    } else {
+        echo "Better luck next time!\n";
+ }
